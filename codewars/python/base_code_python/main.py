af977b260932af0cee7f3cef66e9f89ef5cb109f
jimmy.yuhao <83017426@qq.com>
2020-01-22 19:15:54 +0800

    python test dir added

---
+++ codewars/python/base_code_python/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author  : yuhao
+# @File    : main.py
+# @Software: vim
+import time
+import unittest
+def josephus(items,k):
+
+    #your code here
+
+Test.assert_equals(josephus([1,2,3,4,5,6,7,8,9,10],1),[1,2,3,4,5,6,7,8,9,10])
+Test.assert_equals(josephus([1,2,3,4,5,6,7,8,9,10],2),[2, 4, 6, 8, 10, 3, 7, 1, 9, 5])
+Test.assert_equals(josephus(["C","o","d","e","W","a","r","s"],4),['e', 's', 'W', 'o', 'C', 'd', 'r', 'a'])
+Test.assert_equals(josephus([1,2,3,4,5,6,7],3),[3, 6, 2, 7, 5, 1, 4])
+Test.assert_equals(josephus([],3),[])
+
+if __name__ == "__main__":
+    unittest.main()
