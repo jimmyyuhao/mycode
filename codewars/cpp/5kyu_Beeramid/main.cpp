781c3252992a014097c465f3d93d1c016cdd8ec9
jimmy.yuhao <83017426@qq.com>
2020-01-26 11:36:23 +0800

    5kyu_Beeramid start

---
+++ codewars/cpp/5kyu_Beeramid/main.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+#include <string.h>
+#include <igloo/igloo_alt.h>
+static const size_t npos = -1;
+using namespace igloo;
+using namespace std;
+
+int main(int argc,  char *argv[])
+{
+      return TestRunner::RunAllTests(argc, argv);
+
+}
